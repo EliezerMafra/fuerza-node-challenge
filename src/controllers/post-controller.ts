f51ca450d07034eb404419fe10eae9fa287e02df
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-02 19:19:57 -0300

    Error handling on controller

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -10,59 +10,79 @@ export class PostController{
 	}
 
 	static createPost = (req: Request, res: Response) => {
-		const newPost: PostModel = req.body
+		try{
+			const newPost: PostModel = req.body
 
-		const service = PostController.getService()
-
-		service.createPost(newPost)
-
-		console.log(createMessage(new Date) + 'Creating POST with the params: ' + JSON.stringify(newPost))
-
-		res.status(201).send()
+			const service = PostController.getService()
+	
+			service.createPost(newPost)
+	
+			console.log(createMessage(new Date) + 'Creating POST with the params: ' + JSON.stringify(newPost))
+	
+			res.status(201).send()
+		}catch(err){
+			res.status(400).send(err)
+		}
 	}
 
 	static getAllPosts = async (req: Request, res: Response) => {
-		const pageSize = (typeof req.query.pageSize === 'string') ? parseInt(req.query.pageSize) : 20;
-    	const page = (typeof req.query.page === 'string') ? parseInt(req.query.page) : 0;
-
-		const service = PostController.getService()
-
-		console.log(getMessage(new Date) + 'Getting all POSTS')
-
-		res.status(200).send(await service.getAllPosts(pageSize, page))
+		try{
+			const pageSize = (typeof req.query.pageSize === 'string') ? parseInt(req.query.pageSize) : 20;
+			const page = (typeof req.query.page === 'string') ? parseInt(req.query.page) : 0;
+	
+			const service = PostController.getService()
+	
+			console.log(getMessage(new Date) + 'Getting all POSTS')
+	
+			res.status(200).send(await service.getAllPosts(pageSize, page))
+		}catch(err){
+			res.status(400).send(err)
+		}
 	}
 
 	static getPost = async (req: Request, res: Response) => {
-		const id = req.params.id
-
-		const service = PostController.getService()
-
-		console.log(getMessage(new Date) + 'Getting POSTS with the id: ' + JSON.stringify(id));
-
-		res.status(200).send(await service.getPost(id))
+		try{
+			const id = req.params.id
+	
+			const service = PostController.getService()
+	
+			console.log(getMessage(new Date) + 'Getting POSTS with the id: ' + JSON.stringify(id));
+	
+			res.status(200).send(await service.getPost(id))
+		}catch(err){
+			res.status(400).send(err)
+		}
 	}
 
 	static updatePost = (req: Request, res: Response) => {
-		const id = req.params.id
-		const updatedPost: PostModel = req.body
-
-		const service = PostController.getService()
-
-		service.updatePost(id, updatedPost)
-
-		console.log(updateMessage(new Date) + 'Updating POST with the params: ' + JSON.stringify(updatedPost))
-
-		res.status(202).send()
+		try{
+			const id = req.params.id
+			const updatedPost: PostModel = req.body
+	
+			const service = PostController.getService()
+	
+			service.updatePost(id, updatedPost)
+	
+			console.log(updateMessage(new Date) + 'Updating POST with the params: ' + JSON.stringify(updatedPost))
+	
+			res.status(202).send()
+		}catch(err){
+			res.status(400).send(err)
+		}
 	}
 	static deletePost = (req: Request, res: Response) => {
-		const id = req.params.id
-
-		const service = PostController.getService()
-
-		service.deletePost(id)
-
-		console.log(deleteMessage(new Date) + 'Deleting POST with the params: ' + JSON.stringify(id))
-
-		res.status(202).send()
+		try{
+			const id = req.params.id
+	
+			const service = PostController.getService()
+	
+			service.deletePost(id)
+	
+			console.log(deleteMessage(new Date) + 'Deleting POST with the params: ' + JSON.stringify(id))
+	
+			res.status(202).send()
+		}catch(err){
+			res.status(400).send(err)
+		}
 	}
 }
