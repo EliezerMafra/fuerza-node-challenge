db277514c8320b55be0755f86fc6f00129a64b74
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-03 14:32:25 -0300

    Revert "Update mongoDB-config.ts"
    This reverts commit 579d110139c1cf374b91ab92358525c38d408f71.

--- src/config/database/mongoDB-config.ts
+++ src/config/database/mongoDB-config.ts
@@ -7,7 +7,6 @@ export let mongoDb = mongoose.connection
 export const closeConnectionToMongoDb = () => {mongoDb.close()}
 
 export function stablishMongoDBConnection(){
-	console.log(process.env.DB_URL+"/"+process.env.DB_NAME)
 	mongoose.connect(process.env.DB_URL+"/"+process.env.DB_NAME)
 	mongoDb.on('error', console.log.bind(console, errorMessage(new Date)+'MongoDB connection error'))
 	mongoDb.once('open', () => console.log(connectedMessage(new Date)+'Successfully connected to MongoDB on mongodb+srv://root:'+process.env.ATLAS_PWD+'@fuerzachallenge.in0y8dp.mongodb.net/'+process.env.DB_NAME))
