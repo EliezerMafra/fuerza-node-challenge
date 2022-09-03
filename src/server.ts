e49f6078096d4c1338c5a144d4049a548acba4f7
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-03 14:32:15 -0300

    Revert "Fixing server"
    This reverts commit 3ca4465cb286f022f828da5579a92d67981087a7.

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import { app } from "./app"
 import { stablishMongoDBConnection } from "./config/database/mongoDB-config"
 import { runningMessage } from "./helpers/colorized-messages"
 
-const port = process.env.PORT || 3333
+const port = parseInt(process.env.PORT||'3030')
 
-app.listen(port,  () =>{
-	console.log(runningMessage(new Date)+'Listening on port: ' + port)
+const server_ip = process.env.SERVER_IP || 'localhost'
+
+app.listen(port, server_ip,  () =>{
+	console.log(runningMessage(new Date)+'Listening on http://' + server_ip + ':' + port)
 })
 
 stablishMongoDBConnection()
