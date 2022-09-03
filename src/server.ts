3ca4465cb286f022f828da5579a92d67981087a7
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-03 14:04:46 -0300

    Fixing server

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,10 @@ import { app } from "./app"
 import { stablishMongoDBConnection } from "./config/database/mongoDB-config"
 import { runningMessage } from "./helpers/colorized-messages"
 
-const port = parseInt(process.env.PORT||'3030')
+const port = process.env.PORT || 3333
 
-const server_ip = process.env.SERVER_IP || 'localhost'
-
-app.listen(port, server_ip,  () =>{
-	console.log(runningMessage(new Date)+'Listening on http://' + server_ip + ':' + port)
+app.listen(port,  () =>{
+	console.log(runningMessage(new Date)+'Listening on port: ' + port)
 })
 
 stablishMongoDBConnection()
