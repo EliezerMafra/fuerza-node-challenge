8ef843dc6362df6750d7ff1dc09db8cb2042e216
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-05 00:39:35 -0300

    Fixing mongo DB configuration and .env.example file

--- src/config/database/mongoDB-config.ts
+++ src/config/database/mongoDB-config.ts
@@ -7,7 +7,7 @@ export let mongoDb = mongoose.connection
 export const closeConnectionToMongoDb = () => {mongoDb.close()}
 
 export function stablishMongoDBConnection(){
-	mongoose.connect("mongodb+srv://root:"+process.env.ATLAS_PWD+"@fuerzachallenge.in0y8dp.mongodb.net/"+process.env.DB_NAME)
+	mongoose.connect(process.env.DB_URL || '')
 	mongoDb.on('error', console.log.bind(console, errorMessage(new Date)+'MongoDB connection error'))
-	mongoDb.once('open', () => console.log(connectedMessage(new Date)+'Successfully connected to MongoDB on mongodb+srv://root:'+process.env.ATLAS_PWD+'@fuerzachallenge.in0y8dp.mongodb.net/'+process.env.DB_NAME))
+	mongoDb.once('open', () => console.log(connectedMessage(new Date)+'Successfully connected to MongoDB on '+process.env.DB_URL))
 }
